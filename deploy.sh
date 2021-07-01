bb90001e21a0b09adf8845ff8b9eff0fbd2e08f7
abhaykrishna26 <abhaykak@gmail.com>
2021-07-01 11:18:22 +0530

    dockerfile update in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t abhayk26/multi-client:latest -t abhayk26/multi-client:$SHA -f ./client/Dcokerfile ./client
-docker build -t abhayk26/multi-server:latest -t abhayk26/multi-server:$SHA -f ./server/Dcokerfile ./server
-docker build -t abhayk26/multi-worker:latest -t abhayk26/multi-worker:$SHA -f ./worker/Dcokerfile ./worker
+docker build -t abhayk26/multi-client:latest -t abhayk26/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t abhayk26/multi-server:latest -t abhayk26/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t abhayk26/multi-worker:latest -t abhayk26/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push abhayk26/multi-client:latest
 docker push abhayk26/multi-server:latest
